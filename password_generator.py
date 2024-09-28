4b6d50331f05891bf2fafa7d29cccecc67efaf97
grhnunlu <122951270+grhnunlu@users.noreply.github.com>
2024-09-28 11:48:03 +0200

    Add files via upload

---
+++ password_generator.py
@@ -0,0 +1,18 @@
+import random as r #importing random library for random choosing password
+len_of_password = int(input("How many characters for password: ")) # asking length of password
+chars_all = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM*-+/@#[]{}.,!£$%&=?0123456789"
+chars_lower = "qwertyuiopasdfghjklzxcvbnm" # define the characters for the password
+chars_upper = "QWERTYUIOPASDFGHJKLZXCVBNM"
+chars_special = "*-+/@#[]{}.,!£$%&=?"
+chars_num = "0123456789"
+password = ""
+if len_of_password >= 3: # at least 1 special char, 1 upper char, 1 number
+    for i in range(len_of_password-3):
+        password += chars_all[r.randint(0,len(chars_all)-1)]  # adding random characters to password
+    password += chars_upper[r.randint(0, len(chars_upper) - 1)]
+    password += chars_special[r.randint(0, len(chars_special) - 1)]
+    password += chars_num[r.randint(0, len(chars_num) - 1)]
+else:
+    print("enter a number more or equal to 3")
+password = ''.join(r.sample(password, len(password)))
+print(password)
